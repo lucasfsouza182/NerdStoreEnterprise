361c5ba6482147fdd6547ee53f8bcafea670254e
Lucas Ferreira <lucasfsouza12@gmail.com>
2021-10-01 20:08:46 -0300

    feat: creates auth controller

---
+++ src/services/NSE.Identity.API/Controllers/AuthController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Identity.API.Models;
+
+namespace NSE.Identity.API.Controllers
+{
+    [Route("api/identity")]
+    public class AuthController : Controller
+    {
+
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AuthController(SignInManager<IdentityUser> signInManager,
+                              UserManager<IdentityUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("signin")]
+        public async Task<ActionResult> SignIn(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginUserViewModel loginUser)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return Ok(loginUser);
+            }
+            if (result.IsLockedOut)
+            {                
+                return BadRequest();
+            }
+
+            return BadRequest();
+        }
+    }
+}
