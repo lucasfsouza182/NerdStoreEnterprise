a18cb279ace319e124197402d4346ea1d4a47d15
Lucas Ferreira <lucasfsouza12@gmail.com>
2021-10-03 21:58:24 -0300

    talent: includes user information in token

--- src/services/NSE.Identity.API/Controllers/AuthController.cs
+++ src/services/NSE.Identity.API/Controllers/AuthController.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using NSE.Identity.API.Extensions;
 using NSE.Identity.API.Models;
 
 namespace NSE.Identity.API.Controllers
@@ -13,12 +20,15 @@ namespace NSE.Identity.API.Controllers
 
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly AppSettings _appSettings;
 
         public AuthController(SignInManager<IdentityUser> signInManager,
-                              UserManager<IdentityUser> userManager)
+                              UserManager<IdentityUser> userManager,
+                              IOptions<AppSettings> appSettings)
         {
             _signInManager = signInManager;
             _userManager = userManager;
+            _appSettings = appSettings.Value;
         }
 
         [HttpPost("signin")]
@@ -37,7 +47,7 @@ namespace NSE.Identity.API.Controllers
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, false);
-                return Ok();
+                return Ok(await GenerateJwt(registerUser.Email));
             }
 
             return BadRequest();
@@ -52,14 +62,67 @@ namespace NSE.Identity.API.Controllers
 
             if (result.Succeeded)
             {
-                return Ok(loginUser);
+                return Ok(await GenerateJwt(loginUser.Email));
             }
             if (result.IsLockedOut)
-            {                
+            {
                 return BadRequest();
             }
 
             return BadRequest();
         }
+
+
+        private async Task<LoginResponseViewModel> GenerateJwt(string email)
+        {
+            var user = await _userManager.FindByEmailAsync(email);
+            var claims = await _userManager.GetClaimsAsync(user);
+            var userRoles = await _userManager.GetRolesAsync(user);
+
+            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
+            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), ClaimValueTypes.Integer64));
+
+            foreach (var userRole in userRoles)
+            {
+                claims.Add(new Claim("role", userRole));
+            }
+
+            var identityClaims = new ClaimsIdentity();
+            identityClaims.AddClaims(claims);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            {
+                Issuer = _appSettings.Issuer,
+                Audience = _appSettings.ValidOn,
+                Subject = identityClaims,
+                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpirationHours),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            });
+
+            var encodedToken = tokenHandler.WriteToken(token);
+
+            var response = new LoginResponseViewModel
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpirationHours).TotalSeconds,
+                UserToken = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return response;
+        }
+
+        private static long ToUnixEpochDate(DateTime date)
+            => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
     }
 }
--- src/services/NSE.Identity.API/Models/UserViewModels.cs
+++ src/services/NSE.Identity.API/Models/UserViewModels.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace NSE.Identity.API.Models
 {
@@ -29,4 +30,23 @@ namespace NSE.Identity.API.Models
         public string Password { get; set; }
     }
 
+    public class LoginResponseViewModel
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserTokenViewModel UserToken { get; set; }
+    }
+
+    public class UserTokenViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public IEnumerable<ClaimViewModel> Claims { get; set; }
+    }
+
+    public class ClaimViewModel
+    {
+        public string Value { get; set; }
+        public string Type { get; set; }
+    }
 }
