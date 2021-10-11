f88d268d7e54b3a99adfdb3349680302e3fd7dd9
Lucas Ferreira <lucasfsouza12@gmail.com>
2021-10-11 11:57:05 -0300

    feat: creates product entity

---
+++ src/services/NSE.Catalog.API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NSE.Core.DomainObjects;
+
+namespace NSE.Catalog.API.Models
+{
+    public class Product : Entity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Image { get; set; }
+        public int QuantityStock { get; set; }
+    }
+}
