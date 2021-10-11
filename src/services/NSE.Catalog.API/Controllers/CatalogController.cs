5e757f1f9a8a21addd956056a9fef63e313cd9de
Lucas Ferreira <lucasfsouza12@gmail.com>
2021-10-11 14:38:10 -0300

    refactor: exports startup settings to other files

---
+++ src/services/NSE.Catalog.API/Controllers/CatalogController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSE.Catalog.API.Models;
+
+namespace NSE.Catalog.API.Controllers
+{
+    public class CatalogController
+    {
+        private readonly IProductRepository _productRepository;
+
+        public CatalogController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet("catalogo/produtos")]
+        public async Task<IEnumerable<Product>> Index()
+        {
+            return await _productRepository.GetAll();
+        }
+
+        [HttpGet("catalogo/produtos/{id}")]
+        public async Task<Product> ProductDetail(Guid id)
+        {
+            return await _productRepository.GetById(id);
+        }
+    }
+}
