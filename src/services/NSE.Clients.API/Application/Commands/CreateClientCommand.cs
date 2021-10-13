65109731b24de513f79fcb987bfef1d363f642aa
Lucas Ferreira <lucasfsouza12@gmail.com>
2021-10-13 18:35:57 -0300

    feat: creates create client command

---
+++ src/services/NSE.Clients.API/Application/Commands/CreateClientCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NSE.Core.Messages;
+
+namespace NSE.Clients.API.Application.Commands
+{
+    public class CreateClientCommand : Command
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        public CreateClientCommand(Guid id, string name, string email, string cpf)
+        {
+            AggregateId = id;
+            Id = id;
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+}
