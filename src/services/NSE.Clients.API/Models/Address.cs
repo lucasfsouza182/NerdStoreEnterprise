ec7e4d49b90f0f2bdf1652e32b2abd586dbb1575
Lucas Ferreira <lucasfsouza12@gmail.com>
2021-10-13 14:31:54 -0300

    feat: creates model classes

---
+++ src/services/NSE.Clients.API/Models/Address.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using NSE.Core.DomainObjects;
+
+namespace NSE.Clients.API.Models
+{
+    public class Address : Entity
+    {
+        public string Street { get; private set; }
+        public string Number { get; private set; }
+        public string AddressDetail { get; private set; }
+        public string Neighborhood { get; private set; }
+        public string PostalCode { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+        public Guid ClientId { get; private set; }
+
+        // EF Relation
+        public Client Client { get; protected set; }
+
+        public Address(string street, string number, string addressDetail, string neighborhood, string postalCode, string city, string state, Guid clientId)
+        {
+            Street = street;
+            Number = number;
+            AddressDetail = addressDetail;
+            Neighborhood = neighborhood;
+            PostalCode = postalCode;
+            City = city;
+            State = state;
+            ClientId = clientId;
+        }
+    }
+}
---
+++ src/services/NSE.Clients.API/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿using NSE.Core.DomainObjects;
+
+namespace NSE.Clients.API.Models
+{
+    public class Client : Entity, IAggregateRoot
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+        public bool Deleted { get; private set; }
+        public Address Address { get; private set; }
+
+        // EF Relation
+        protected Client() { }
+
+        public Client(string name, string email, string cpf)
+        {
+            Name = name;
+            Email = email;
+            Cpf = cpf;
+            Deleted = false;
+        }
+    }
+}
