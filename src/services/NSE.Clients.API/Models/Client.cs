7c578f311cb2f5fc84da41ec900aecb6b52bb3bc
Lucas Ferreira <lucasfsouza12@gmail.com>
2021-10-13 14:58:53 -0300

    feat: creates value objects cpf and email

--- src/services/NSE.Clients.API/Models/Client.cs
+++ src/services/NSE.Clients.API/Models/Client.cs
@@ -1,24 +1,36 @@
-﻿using NSE.Core.DomainObjects;
+﻿using System;
+using NSE.Core.DomainObjects;
 
 namespace NSE.Clients.API.Models
 {
     public class Client : Entity, IAggregateRoot
     {
         public string Name { get; private set; }
-        public string Email { get; private set; }
-        public string Cpf { get; private set; }
+        public Email Email { get; private set; }
+        public Cpf Cpf { get; private set; }
         public bool Deleted { get; private set; }
         public Address Address { get; private set; }
 
         // EF Relation
         protected Client() { }
 
-        public Client(string name, string email, string cpf)
+        public Client(Guid id, string name, string email, string cpf)
         {
+            Id = id;
             Name = name;
-            Email = email;
-            Cpf = cpf;
+            Email = new Email(email);
+            Cpf = new Cpf(cpf);
             Deleted = false;
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = new Email(email);
+        }
+
+        public void setAddress(Address address)
+        {
+            Address = address;
         }
     }
 }
