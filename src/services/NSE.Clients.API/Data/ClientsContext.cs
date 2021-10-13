d5f67865aff37eed9d0a9b002f4341bb1373776c
Lucas Ferreira <lucasfsouza12@gmail.com>
2021-10-13 15:36:57 -0300

    feat: creates clients context

---
+++ src/services/NSE.Clients.API/Data/ClientsContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NSE.Clients.API.Models;
+using NSE.Core.Data;
+
+namespace NSE.Clients.API.Data
+{
+    public class ClientsContext : DbContext, IUnitOfWork
+    {
+        public ClientsContext(DbContextOptions<ClientsContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        DbSet<Client> clients { get; set; }
+        DbSet<Address> addresses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientsContext).Assembly);
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
---
+++ src/services/NSE.Clients.API/Data/Mappings/ClientMapping.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NSE.Clients.API.Models;
+using NSE.Core.DomainObjects;
+
+namespace NSE.Clients.API.Data.Mappings
+{
+    public class ClientMapping : IEntityTypeConfiguration<Client>
+    {
+        public void Configure(EntityTypeBuilder<Client> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.OwnsOne(c => c.Cpf, tf =>
+            {
+                tf.Property(c => c.Number)
+                    .IsRequired()
+                    .HasMaxLength(Cpf.CpfMaxLength)
+                    .HasColumnName("Cpf")
+                    .HasColumnType($"varchar({Cpf.CpfMaxLength})");
+            });
+
+            builder.OwnsOne(c => c.Email, tf =>
+            {
+                tf.Property(c => c.Address)
+                    .IsRequired()
+                    .HasColumnName("Email")
+                    .HasColumnType($"varchar({Email.AddressMaxLength})");
+            });
+
+            // 1 : 1 => Client : Address
+            builder.HasOne(c => c.Address)
+                .WithOne(c => c.Client);
+
+            builder.ToTable("Clients");
+        }
+    }
+}
