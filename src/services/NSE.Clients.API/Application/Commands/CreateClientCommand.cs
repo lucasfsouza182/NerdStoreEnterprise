b9e14ade07b06476af6f4b3ea5f6d5fe2725cf77
Lucas Ferreira <lucasfsouza12@gmail.com>
2021-10-16 18:51:01 -0300

    feat: creates command validation

--- src/services/NSE.Clients.API/Application/Commands/CreateClientCommand.cs
+++ src/services/NSE.Clients.API/Application/Commands/CreateClientCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentValidation;
 using NSE.Core.Messages;
 
 namespace NSE.Clients.API.Application.Commands
@@ -17,6 +18,45 @@ namespace NSE.Clients.API.Application.Commands
             Name = name;
             Email = email;
             Cpf = cpf;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new CreateClientValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+
+    public class CreateClientValidation : AbstractValidator<CreateClientCommand>
+    {
+        public CreateClientValidation()
+        {
+            RuleFor(c => c.Id)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id invalid");
+
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .WithMessage("The customer's name was not valid");
+
+            RuleFor(c => c.Cpf)
+                .Must(HasValidCpf)
+                .WithMessage("The CPF is not valid.");
+
+            RuleFor(c => c.Email)
+                .Must(HasValidEmail)
+                .WithMessage("The e-mail is not valid.");
+            
+        }
+
+        protected static bool HasValidCpf(string cpf)
+        {
+            return Core.DomainObjects.Cpf.Validate(cpf);
+        }
+
+        protected static bool HasValidEmail(string email)
+        {
+            return Core.DomainObjects.Email.Validate(email);
         }
     }
 }
